2fd7fc246e0c4edcfb23ee9246c2b5e8f1bd552a
CarolinaNSilva <122793746+CarolinaNSilva@users.noreply.github.com>
2023-03-14 11:12:11 -0300

    ajustando js

--- valida_vinculo.js
+++ valida_vinculo.js
@@ -1,61 +1,80 @@
 var Portaria = function () {
-    this.createPessoa = function (type) {
-        var pessoa;
-  
-        if (type === "1") {
-            pessoa = new Diretor();
-        } else if (type === "2") {
-          pessoa = new Coordenador();
-        } else if (type === "3") {
-          pessoa = new Professor();
-        } else if (type === "4") {
-          pessoa = new Adiministrativo();
-        }else if (type === "5") {
-          pessoa = new Aluno();
-        }else if (type === "6") {
-          pessoa = new Vestibulando();
-        }else {
-          pessoa = new Ninguem();
-        }
-  
-        pessoa.type = type;
-  
-        pessoa.say = function () {
-            console.log(this.type + ": rate " + this.hourly + "/hour");
-        }
-  
-        return employee;
-    }
+  this.createPessoa = function (type) {
+      var pessoa;
+
+      if (type === "1") {
+          pessoa = new Diretor();
+      } else if (type === "2") {
+        pessoa = new Coordenador();
+      } else if (type === "3") {
+        pessoa = new Professor();
+      } else if (type === "4") {
+        pessoa = new Adiministrativo();
+      }else if (type === "5") {
+        pessoa = new Aluno();
+      }else if (type === "6") {
+        pessoa = new Vestibulando();
+      }else {
+        pessoa = new Semvinculo();
+      }
+
+      pessoa.type = type;
+
+      pessoa.say = function () {
+          console.log(this.type + "v " + this.hourly + "e");
+      }
+
+      return pessoa;
   }
-  
-  var Diretor = function () {
-    this.hourly = "$12";
-  };
-  
-  var PartTime = function () {
-    this.hourly = "$11";
-  };
-  
-  var Temporary = function () {
-    this.hourly = "$10";
-  };
-  
-  var Contractor = function () {
-    this.hourly = "$15";
-  };
-  
-  function run() {
-  
-    var pessoa = [];
-    var factory = new Factory();
-  
-    employees.push(factory.createEmployee("fulltime"));
-    employees.push(factory.createEmployee("parttime"));
-    employees.push(factory.createEmployee("temporary"));
-    employees.push(factory.createEmployee("contractor"));
-  
-    for (var i = 0, len = employees.length; i < len; i++) {
-        employees[i].say();
-    }
-  }
-  
+}
+
+var Diretor = function () {
+  this.cargo = "Diretor";
+};
+
+var Coordenador = function () {
+  this.cargo = "Coordenador";
+};
+
+var Professor = function () {
+  this.cargo = "Professor";
+};
+
+var Adiministrativo = function () {
+  this.cargo = "Adiministrativo";
+};
+
+var Aluno = function () {
+  this.cargo = "Aluno";
+};
+
+var Vestibulando = function () {
+  this.cargo = "Vestibulando";
+};
+
+var Semvinculo = function () {
+  this.cargo = " Sem vinculo";
+};
+
+  console.log("\nSeja Bem-vindo a Portaria FATEC!\nPara liberar acesso informe seu vinculo com a instituição")
+
+  console.log(`
+  Vinculo         código
+----------------------------
+  Diretor:           1
+  Coordenador:       2
+  Professor:         3
+  Adiministrativo:   4
+  Aluno:             5 
+  Vestibulando:      6
+  Outro:             0
+`)
+function run() {
+
+var portaria = new Portaria();
+var cod = "1";
+var pessoa = portaria.createPessoa(cod);
+
+console.log(pessoa.cargo)
+}
+run()
